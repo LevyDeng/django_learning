f710ea74a3c8a265bcc6aef1c86ab13f3a11377a
ptrees <ptrees@sina.cn>
2017-02-26 13:33:30 +0800

    '1.2'

--- models_learn/people/admin.py
+++ models_learn/people/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article,Person
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','update_time')
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Person)
--- models_learn/people/models.py
+++ models_learn/people/models.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -6,3 +7,14 @@ from django.db import models
 class Person(models.Model):
     name=models.CharField(max_length=30)
     age=models.IntegerField()
+    def __unicode__(self):
+        return self.name
+
+class Article(models.Model):
+    title=models.CharField(u'标题',max_length=256)
+    auther=models.CharField(u'作者',max_length=64)
+    content=models.TextField(u'内容')
+    pub_date=models.DateTimeField(u'发表时间',auto_now_add=True,editable=True)
+    update_time=models.DateTimeField(u'修改时间',auto_now=True,null=True)
+    def __unicode__(self):
+        return self.title
